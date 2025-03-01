4b28693f98ef7622f0472ce8b30e1faad9417113
ivan7182 <muhammadivan7182@gmail.com>
2025-03-01 19:49:23 +0700

    DasarGo

--- function_as_parameter.go
+++ function_as_parameter.go
@@ -5,36 +5,31 @@ import (
 	"strings"
 )
 
-// func sayHello(name string, filter func(string) string) {
-// 	filteredName := filter(name)
-// 	fmt.Println("hello", filteredName)
-// }
+type filter func(string) string
+type fungsiOperasi func(int) int
 
-// func spamFilter(name string) string {
-// 	if name == "anjing" {
-// 		return "***"
-// 	} else {
-// 		return name
-// 	}
-// }
+func sayHello(name string, filter filter) {
+	filteredName := filter(name)
+	fmt.Println("hello", filteredName)
+}
 
-// func main() {
-// 	sayHello("anjing", spamFilter)
-// }
+func spamFilter(name string) string {
+	if name == "anjing" {
+		return "***"
+	} else {
+		return name
+	}
+}
 
-// func prosesAngka(angka int, fungsiOperasi func(int) int) {
-// 	hasil := fungsiOperasi(angka)
-// 	fmt.Println(hasil)
+func prosesAngka(angka int, fungsiOperasi fungsiOperasi) {
+	hasil := fungsiOperasi(angka)
+	fmt.Println(hasil)
 
-// }
+}
 
-// func kaliDua(n int) int {
-// 	return n * 2
-// }
-
-// func main() {
-// 	prosesAngka(5, kaliDua)
-// }
+func kaliDua(n int) int {
+	return n * 2
+}
 
 func prosesTeks(teks string, fungsiModifikasi func(string) string) {
 	hasil := fungsiModifikasi(teks)
@@ -46,6 +41,41 @@ func keKapital(s string) string {
 	return strings.ToUpper(s)
 }
 
+func cekUmur(umur int, validator func(int) bool) {
+	if validator(umur) {
+		fmt.Println("dewasa")
+	} else {
+		fmt.Println("belum cukup dewasa")
+	}
+}
+
+func minimal18(umur int) bool {
+	return umur <= 18
+}
+
+func hitungDisckon(harga float64, dickonCalculator func(float64) float64) {
+	hasil := dickonCalculator(harga)
+	fmt.Println(hasil)
+}
+
+func diskon10(harga float64) float64 {
+	return harga * 0.9
+}
+
+func prosesNama(nama string, formatter func(string) string) {
+	hasil := formatter(nama)
+	fmt.Println(hasil)
+}
+
+func tambahMR(nama string) string {
+	return "MR" + nama
+}
+
 func main() {
-	prosesTeks("golang", keKapital)
+	// sayHello("anjing", spamFilter)
+	//prosesTeks("golang", keKapital)
+	// cekUmur(10, minimal18)
+	hitungDisckon(100000, diskon10)
+	prosesNama("ivan", tambahMR)
+	// prosesAngka(5, kaliDua)
 }
