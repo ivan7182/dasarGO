bca5903686d562221e2593fc2acfcb6e1dcd1599
ivan7182 <muhammadivan7182@gmail.com>
2025-03-02 22:46:51 +0700

    DasarGo

---
+++ struct.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Customer struct {
+	Name, Address string
+	Age           int
+}
+
+type Mahasiswa struct {
+	Nama, NIM string
+	IPK       float64
+}
+
+func main() {
+	var ivan Customer
+	ivan.Name = "Ivan"
+	ivan.Address = "Tegal"
+	ivan.Age = 24
+	fmt.Println(ivan)
+	fmt.Println(ivan.Name)
+
+	joko := Customer{
+		Name:    "joko",
+		Address: "tegal",
+		Age:     22,
+	}
+	fmt.Println(joko)
+
+	budi := Customer{"budi", "jakarta", 22}
+	fmt.Println(budi)
+
+	satria := Mahasiswa{
+		Nama: "staria",
+		NIM:  "19090082",
+		IPK:  14.4,
+	}
+	fmt.Println(satria)
+}
